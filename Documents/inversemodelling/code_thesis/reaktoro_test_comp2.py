d7963b1ffad461d5e9f79dafbca48080bfb95bbe
jdkloosterman <j.d.kloosterman@student.tudelft.nl>
2021-11-25 13:04:23 +0100

    OBM approach

---
+++ Documents/inversemodelling/code_thesis/reaktoro_test_comp2.py
@@ -0,0 +1,98 @@
+''''Testing reaktoro'''
+import matplotlib.pyplot as plt
+import numpy as np
+from reaktoro import *
+
+'''Components to elements'''
+
+# TODO:
+#  input components,                                            Y
+#  get them to elements with elimination matrix,                NOT, USED ELEMENTAMOUNTS()
+#  Perform reaction with reaktoro,                              CHECK, WITH ELEMENTS
+#  output density,                                              Y
+#  and component distribution,                                  Y
+#  Try in 1D vector code, with injection and producer well,     Y
+#  OBL                                                          Y
+
+#############################################
+# Instead of using problem, a new ChemicalState is given. This makes it easier to compute things such as
+# mole_total = ChemicalQuantity(state).value("phaseAmount(Aqueous)")
+#############################################
+
+###INPUT###
+class Variables():
+    def __init__(self):
+        self.p = 100 # in bar
+        self.T = 60 # in celsius
+        self.mole_h20 = 100
+        self.mole_ca = 5
+        self.mole_CO3 = 5
+        number_cells = 100
+        self.cells = np.arange(0, number_cells, 1)
+        self.mole_cells_co2 = np.linspace(5, 0, len(self.cells))
+
+class Model(Variables):
+    def __init__(self):
+        super().__init__()
+        self.editor = ChemicalEditor(Database('supcrt98.xml'))
+        self.editor.addAqueousPhase("H2O(l) CO2(aq) Ca++ CO3-- CaCO3(aq)")
+        self.system = ChemicalSystem(self.editor)
+        self.solver = EquilibriumSolver(self.system)  # solves the system
+        self.states = []
+        # Two version can be coded with the same solution. One uses the EquilibriumProblem function,
+        # Other will use the state function. Both need to output a state in order to be solved by EquilibriumSolver
+
+    def addingproblem(self, temp, pres, mole_comp):
+        self.problem = EquilibriumProblem(self.system)
+        self.problem.setTemperature(temp, 'celsius')
+        self.problem.setPressure(pres, 'bar')
+        self.problem.add('H2O', self.mole_h20, 'mol')
+        self.problem.add('Ca++', self.mole_ca, 'mol')
+        self.problem.add('CO3--', self.mole_CO3, 'mol')
+        self.problem.add('CO2', mole_comp, 'mol')
+        self.state = equilibrate(self.problem) # Equilibrate the problem in order to write to state
+        self.states.append(self.state)
+
+    def addingstates(self, temp,pres,mole_comp):
+        self.state = ChemicalState(self.system)
+        self.state.setTemperature(temp, 'celsius')
+        self.state.setPressure(pres, 'bar')
+        self.state.setSpeciesAmount('H2O(l)', self.mole_h20, 'mol')
+        self.state.setSpeciesAmount('Ca++', self.mole_ca, 'mol')
+        self.state.setSpeciesAmount('CO3--', self.mole_CO3, 'mol')
+        self.state.setSpeciesAmount('CO2(aq)', mole_comp, 'mol')
+        self.states.append(self.state)
+
+    def ouput(self):
+        CO2_amount = [state.speciesAmount("CO2(aq)") for state in self.states]
+        CaCO3_amount = [state.speciesAmount("CaCO3(aq)") for state in self.states]
+        Ca_amount = [state.speciesAmount("Ca++") for state in self.states]
+        CO3_amount = [state.speciesAmount("CO3--") for state in self.states]
+        mass = [ChemicalQuantity(state).value("phaseMass(Aqueous)") for state in self.states]
+        volume = [ChemicalQuantity(state).value("phaseVolume(Aqueous)") for state in self.states]
+        density = np.zeros(len(mass))
+        for i in range(len(mass)):
+            density[i] = mass[i] / volume[i]
+        return CO2_amount, CaCO3_amount, Ca_amount, CO3_amount, density
+
+m = Model()
+for i in range(len(m.cells)):
+    # m.addingproblem(m.T, m.p, m.mole_cells_co2[i])
+    m.addingstates(m.T, m.p, m.mole_cells_co2[i])
+for i in range(len(m.cells)):
+    amount_elements = m.states[i].elementAmounts()
+    m.solver.solve(m.states[i], m.T, m.p, amount_elements)
+
+CO2_amount, CaCO3_amount, Ca_amount, CO3_amount, density = m.ouput()
+
+plt.figure(1)
+plt.plot(m.cells, density, label='Density')
+plt.legend()
+plt.show()
+plt.figure(2)
+plt.plot(m.cells, CO2_amount, label='CO2')
+plt.plot(m.cells, CaCO3_amount, label='CaCO3(aq)')
+plt.plot(m.cells, Ca_amount, label='Ca++')
+plt.plot(m.cells, CO3_amount, label='CO3--')
+plt.legend()
+plt.show()

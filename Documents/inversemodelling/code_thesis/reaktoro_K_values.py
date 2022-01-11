ebd213249522192eb8fea54a9f8d1ca18d336245
JKloost <84318580+JKloost@users.noreply.github.com>
2022-01-11 11:00:42 +0100

    Update reactions

--- Documents/inversemodelling/code_thesis/reaktoro_K_values.py
+++ Documents/inversemodelling/code_thesis/reaktoro_K_values.py
@@ -5,9 +5,9 @@ T = 320 # K
 p = 1e7 # Pa
 
 
-reac1 = 'H2O(l) = OH- + H+'
-reac2 = 'CO2(aq) + H2O(l) = HCO3- + H+'
-reac3 = 'HCO3- = CO3-- + H+'
+reac1 = 'H+ + OH- = H2O(l)'
+reac2 = 'HCO3- + H+ = CO2(aq) + H2O(l)'
+reac3 = 'CO3-- + H+ = HCO3-'
 reac4 = 'CaCO3(aq) + H+ = Ca++ + HCO3-'
 print('reac1',thermo.lnEquilibriumConstant(T, p, reac1).val)
 print('reac2',thermo.lnEquilibriumConstant(T, p, reac2).val)
